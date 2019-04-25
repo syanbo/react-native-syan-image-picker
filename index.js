b1f06c98339ab49702b5dd67a6d037334b6987e0
少言 <303152878@qq.com>
2019-04-25 22:23:48 +0800

    change 优化安卓base64

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ const defaultOptions = {
     circleCropRadius: ~~(width / 4), // 圆形裁剪半径，默认屏幕宽度一半
     showCropFrame: true,       // 是否显示裁剪区域，默认true
     showCropGrid: false,       // 是否隐藏裁剪区域网格，默认false
+    compress: true,
+    minimumCompressSize: 100,  // 小于100kb的图片不压缩
     quality: 90,               // 压缩质量
     enableBase64: false,       // 是否返回base64编码，默认不返回
     allowPickingOriginalPhoto: false,
